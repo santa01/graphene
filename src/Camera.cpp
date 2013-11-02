a87d2ed79f0ff1a3d3f682f2f8b7a217ceb9a45e
Pavlo Lavrenenko <santa.ssh@gmail.com>
2013-11-02 19:17:55 +0200

    Add Camera and Viewport classes

---
+++ src/Camera.cpp
@@ -0,0 +1,120 @@
+/*
+ * Copyright (c) 2013 Pavlo Lavrenenko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <Camera.h>
+#include <Mat3.h>
+#include <Quaternion.h>
+#include <cmath>
+
+namespace Graphene {
+
+void Camera::rotate(const Math::Vec3& vector, float angle) {
+    if (vector == Math::Vec3::ZERO) {
+        return;
+    }
+
+    Math::Vec3 axis(vector);
+    Math::Quaternion q(axis.normalize(), angle * M_PI / 180.0f);
+    q.normalize();
+
+    Math::Mat3 rotationMatrix = q.extractMat4().extractMat3();
+    Math::Vec3 up = this->getUp();
+    Math::Vec3 target = this->getTarget();
+
+    up = rotationMatrix * up;
+    target = rotationMatrix * target;
+
+    Math::Vec3 right = target.cross(up);
+    right.normalize();
+
+    float xAngleNew, yAngleNew, zAngleNew;
+    q.extractEulerAngles(xAngleNew, yAngleNew, zAngleNew);
+
+    this->xAngle = xAngleNew * 180.f / M_PI;
+    this->yAngle = yAngleNew * 180.f / M_PI;
+    this->zAngle = zAngleNew * 180.f / M_PI;
+
+    this->updateRotation(right, up, target);
+}
+
+void Camera::lookAt(const Math::Vec3& vector) {
+    if (vector == Math::Vec3::ZERO) {
+        return;
+    }
+
+    Math::Vec3 target(-vector);
+    target.normalize();
+
+    Math::Vec3 right = this->getRight();
+    if (target != Math::Vec3::UNIT_Y && target != -Math::Vec3::UNIT_Y) {
+        right = target.cross(Math::Vec3::UNIT_Y);
+        right.normalize();
+    }
+
+    Math::Vec3 up = right.cross(target);
+    up.normalize();
+
+    this->updateRotation(right, up, target);
+}
+
+void Camera::updateProjection(ProjectionType projectionType) {
+    this->projection = Math::Mat4();
+    this->projectionType = projectionType;
+
+    switch (this->projectionType) {
+        case TYPE_PERSPECTIVE:
+            this->projection.set(0, 0, 1.0f / (tanf(this->fov * M_PI / 180.0f / 2.0f) *
+                                       this->aspectRatio));
+            this->projection.set(1, 1, 1.0f / (tanf(this->fov * M_PI / 180.0f / 2.0f)));
+            this->projection.set(2, 2, (-this->farPlane - this->nearPlane) /
+                                       (this->farPlane - this->nearPlane));
+            this->projection.set(2, 3, (-2.0f * this->farPlane * this->nearPlane) /
+                                       (this->farPlane - this->nearPlane));
+            this->projection.set(3, 2, -1.0f);
+            this->projection.set(3, 3, 0.0f);
+            break;
+        case TYPE_ORTHOGRAPHIC:
+            this->projection.set(0, 0, 1.0f / ((this->farPlane - this->nearPlane) *
+                                       this->aspectRatio));
+            this->projection.set(1, 1, 1.0f / (this->farPlane - this->nearPlane));
+            this->projection.set(2, 2, -2.0f / (this->farPlane - this->nearPlane));
+            this->projection.set(2, 3, (-this->farPlane - this->nearPlane) /
+                                       (this->farPlane - this->nearPlane));
+            break;
+    }
+}
+
+void Camera::updateRotation(const Math::Vec3& right, const Math::Vec3& up, const Math::Vec3& target) {
+    this->rotation.set(0, 0, right.get(Math::Vec3::X));
+    this->rotation.set(0, 1, right.get(Math::Vec3::Y));
+    this->rotation.set(0, 2, right.get(Math::Vec3::Z));
+
+    this->rotation.set(1, 0, up.get(Math::Vec3::X));
+    this->rotation.set(1, 1, up.get(Math::Vec3::Y));
+    this->rotation.set(1, 2, up.get(Math::Vec3::Z));
+
+    this->rotation.set(2, 0, target.get(Math::Vec3::X));
+    this->rotation.set(2, 1, target.get(Math::Vec3::Y));
+    this->rotation.set(2, 2, target.get(Math::Vec3::Z));
+}
+
+}  // namespace Graphene
---
+++ src/Camera.h
@@ -0,0 +1,185 @@
+/*
+ * Copyright (c) 2013 Pavlo Lavrenenko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include <Mat4.h>
+
+namespace Graphene {
+
+class Camera {
+public:
+    enum ProjectionType {
+        TYPE_PERSPECTIVE,
+        TYPE_ORTHOGRAPHIC
+    };
+
+    Camera() {
+        this->aspectRatio = 1.3333f;
+        this->nearPlane = 0.1f;
+        this->farPlane = 100.0f;
+        this->fov = 70.0f;
+
+        this->updateProjection(TYPE_PERSPECTIVE);
+        this->lookAt(Math::Vec3::UNIT_Z);
+    }
+
+    Camera(float x, float y, float z):
+            Camera() {
+        this->setPosition(x, y, z);
+    }
+
+    Camera(const Math::Vec3& position):
+            Camera() {
+        this->setPosition(position);
+    }
+
+    void setPosition(float x, float y, float z) {
+        this->setPosition(Math::Vec3(x, y, z));
+    }
+
+    void setPosition(const Math::Vec3& position) {
+        this->translation.set(0, 3, -position.get(Math::Vec3::X));
+        this->translation.set(1, 3, -position.get(Math::Vec3::Y));
+        this->translation.set(2, 3, -position.get(Math::Vec3::Z));
+    }
+
+    Math::Vec3 getPosition() const {
+        return Math::Vec3(-this->translation.get(0, 3), -this->translation.get(1, 3), -this->translation.get(2, 3));
+    }
+
+    float getXAngle() const {
+        return this->xAngle;
+    }
+
+    float getYAngle() const {
+        return this->yAngle;
+    }
+
+    float getZAngle() const {
+        return this->zAngle;
+    }
+
+    void roll(float angle) {
+        this->rotate(Math::Vec3::UNIT_X, angle);
+    }
+
+    void yaw(float angle) {
+        this->rotate(Math::Vec3::UNIT_Y, angle);
+    }
+
+    void pitch(float angle) {
+        this->rotate(Math::Vec3::UNIT_Z, angle);
+    }
+
+    void rotate(const Math::Vec3& vector, float angle);
+
+    ProjectionType getProjectionType() const {
+        return this->projectionType;
+    }
+
+    void setProjectionType(ProjectionType type) {
+        this->updateProjection(type);
+    }
+
+    float getAspectRatio() const {
+        return this->aspectRatio;
+    }
+
+    void setAspectRatio(float aspectRatio) {
+        this->aspectRatio = aspectRatio;
+        this->updateProjection(this->projectionType);
+    }
+
+    float getNearPlane() const {
+        return this->nearPlane;
+    }
+
+    void setNearPlane(float nearPlane) {
+        this->nearPlane = nearPlane;
+        this->updateProjection(this->projectionType);
+    }
+
+    float getFarPlane() const {
+        return this->farPlane;
+    }
+
+    void setFarPlane(float farPlane) {
+        this->farPlane = farPlane;
+        this->updateProjection(this->projectionType);
+    }
+
+    float getFov() const {
+        return this->fov;
+    }
+
+    void setFov(float fov) {
+        this->fov = fov;
+        this->updateProjection(this->projectionType);
+    }
+
+    Math::Vec3 getUp() const {
+        return Math::Vec3(this->rotation.get(1, 0), this->rotation.get(1, 1), this->rotation.get(1, 2));
+    }
+
+    Math::Vec3 getTarget() const {
+        return -Math::Vec3(this->rotation.get(2, 0), this->rotation.get(2, 1), this->rotation.get(2, 2));
+    }
+
+    Math::Vec3 getRight() const {
+        Math::Vec3 up(this->rotation.get(1, 0), this->rotation.get(1, 1), this->rotation.get(1, 2));
+        Math::Vec3 target(this->rotation.get(2, 0), this->rotation.get(2, 1), this->rotation.get(2, 2));
+        Math::Vec3 right = target.cross(up);
+
+        return right.normalize();
+    }
+
+    void lookAt(float x, float y, float z) {
+        this->lookAt(Math::Vec3(x, y, z));
+    }
+
+    void lookAt(const Math::Vec3& target);
+
+private:
+    void updateProjection(ProjectionType projectionType);
+    void updateRotation(const Math::Vec3& right, const Math::Vec3& up, const Math::Vec3& target);
+
+    ProjectionType projectionType;
+
+    Math::Mat4 projection;
+    Math::Mat4 translation;
+    Math::Mat4 rotation;
+
+    float aspectRatio;
+    float nearPlane;
+    float farPlane;
+    float fov;
+
+    float xAngle;
+    float yAngle;
+    float zAngle;
+};
+
+}  // namespace Graphene
+
+#endif  // CAMERA_H
