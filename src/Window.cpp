59b2a8eb0ff393e3db5a4fae3bc07166beb646e1
Pavlo Lavrenenko <santa.ssh@gmail.com>
2014-05-03 23:14:37 +0300

    Add optional OpenGL debug and profiling

--- src/Window.cpp
+++ src/Window.cpp
@@ -24,8 +24,38 @@
 #include <stdexcept>
 #include <algorithm>
 #include <iterator>
+#ifdef GRAPHENE_DEBUG
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#endif
 
 namespace Graphene {
+
+#ifdef GRAPHENE_DEBUG
+std::unordered_map<GLenum, std::string> sourceMap = {
+    { GL_DEBUG_SOURCE_API_ARB,             "OpenGL" },
+    { GL_DEBUG_SOURCE_SHADER_COMPILER_ARB, "GLSL" },
+    { GL_DEBUG_SOURCE_WINDOW_SYSTEM_ARB,   "Window system" },
+    { GL_DEBUG_SOURCE_THIRD_PARTY_ARB,     "3rd party" },
+    { GL_DEBUG_SOURCE_APPLICATION_ARB,     "Application" },
+    { GL_DEBUG_SOURCE_OTHER_ARB,           "Other" }
+};
+
+std::unordered_map<GLenum, std::string> typeMap = {
+    { GL_DEBUG_TYPE_ERROR_ARB,               "Error" },
+    { GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR_ARB, "Deprecated" },
+    { GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR_ARB,  "Indefined" },
+    { GL_DEBUG_TYPE_PERFORMANCE_ARB,         "Performance" },
+    { GL_DEBUG_TYPE_PORTABILITY_ARB,         "Portability" },
+    { GL_DEBUG_TYPE_OTHER_ARB,               "Other" }
+};
+
+extern "C" void debugHandler(GLenum source, GLenum type, GLuint id, GLenum severity,
+        GLsizei length, const GLchar* message, GLvoid* userParam) {
+    std::cout << typeMap[type] << " [" << sourceMap[source] << "]: "<< message << std::endl;
+}
+#endif
 
 Window::Window(int width, int height):
         RenderTarget(width, height) {
@@ -42,7 +72,11 @@ Window::Window(int width, int height):
 
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+#ifdef GRAPHENE_DEBUG
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG | SDL_GL_CONTEXT_DEBUG_FLAG);
+#else
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG);
+#endif
 
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
@@ -65,6 +99,16 @@ Window::Window(int width, int height):
     if (glewInit() != GLEW_OK) {
         throw std::runtime_error("Failed to initialize GLEW");
     }
+
+#ifdef GRAPHENE_DEBUG
+    if (glewIsSupported("GL_ARB_debug_output")) {
+        glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS_ARB);
+        glDebugMessageControlARB(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, nullptr, true);
+        glDebugMessageCallbackARB(debugHandler, nullptr);
+    } else {
+        std::cout << "GL_ARB_debug_output unavailable, debug disabled" << std::endl;
+    }
+#endif
 
     glFrontFace(GL_CW);
     glBlendFunc(GL_ONE, GL_ONE);
