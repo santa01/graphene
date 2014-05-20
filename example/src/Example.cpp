712e2ceade4cc30edf7eefa8734e796af67f6704
Pavlo Lavrenenko <santa.ssh@gmail.com>
2014-05-20 22:41:25 +0300

    Update example application
    Update code for recent library changes.
    Ass FPS-alike camera movement.

--- example/src/Example.cpp
+++ example/src/Example.cpp
@@ -1,68 +1,112 @@
-#include <Graphene.h>
-#include <functional>
+/*
+ * Copyright (c) 2013 Pavlo Lavrenenko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
-static bool isRunning = true;
+#include <Example.h>
+#include <Vec3.h>
 
-static void mouseHandler(const SDL_Event* event) {}
-static void keyHandler(const SDL_Event* event) {}
-
-static void quitHandler(const SDL_Event* event) {
-    isRunning = false;
-}
-
-int main() {
-    /* Create engine instance */
-
-    Graphene::Graphene engine(800, 600);
-
-    /* Setup event handlers */
-
-    auto window = engine.getWindow();
-    window->onMouseEvent.connect(std::bind(mouseHandler, std::placeholders::_1));
-    window->onKeyEvent.connect(std::bind(keyHandler, std::placeholders::_1));
-    window->onQuitEvent.connect(std::bind(quitHandler, std::placeholders::_1));
+Example::Example(int width, int height):
+        Graphene::Engine(width, height) {
 
     /* Setup scene */
 
-    auto sceneManager = *engine.getSceneManagers().begin();
+    auto sceneManager = *this->getSceneManagers().begin();
     sceneManager->setAmbientEnergy(0.2f);
     sceneManager->setLightPass(true);
 
-    auto node1 = sceneManager->createNode();
-    sceneManager->getRootNode()->attachNode(node1);
+    this->player = sceneManager->createNode();
+    sceneManager->getRootNode()->attachNode(this->player);
 
-    auto node2 = sceneManager->createNode();
-    sceneManager->getRootNode()->attachNode(node2);
+    auto node = sceneManager->createNode();
+    sceneManager->getRootNode()->attachNode(node);
 
     /* Populate scene with objects */
 
-    auto objectManager = *engine.getObjectManagers().begin();
+    auto objectManager = *this->getObjectManagers().begin();
 
-    auto camera = objectManager->createCamera();
-    node1->attachObject(camera);
+    this->camera = objectManager->createCamera();
+    this->player->attachObject(this->camera);
 
     auto light = objectManager->createLight();
-    node2->attachObject(light);
+    node->attachObject(light);
 
     auto entity = objectManager->createEntity("assets/example.entity");
-    node2->attachObject(entity);
+    node->attachObject(entity);
 
     /* Update default viewport with camera */
 
-    auto viewport = *engine.getWindow()->getViewports().begin();
+    auto viewport = *this->getWindow()->getViewports().begin();
     viewport->setCamera(camera);
 
     /* Update scene */
 
-    node1->translate(2.0f, 2.0f, 3.0f);
-    light->translate(0.0f, 0.0f, 2.0f);
-    camera->lookAt(-2.0f, -2.0f, -3.0f);
+    this->player->translate(0.0f, 0.0f, -3.0f);
+    light->translate(0.0f, 2.0f, 0.0f);
 
-    /* Run renderer */
+    /* Keep mouse inside the window */
 
-    while (isRunning) {
-        engine.update();
+    this->getWindow()->captureMouse(true);
+}
+
+void Example::onMouseMotion(int x, int y) {
+    float xAngle = y * MOUSE_SPEED;
+    static float roll = 0.0f;
+
+    if (roll + xAngle > 90.0f) {
+        xAngle = 90.0f - roll;
+    } else if (roll + xAngle < -90.0f) {
+        xAngle = -90.0f - roll;
+    }
+    roll += xAngle;
+
+    this->player->yaw(x * MOUSE_SPEED);
+    if (xAngle != 0) {
+        this->player->rotate(this->camera->getRight(), xAngle);
+    }
+}
+
+void Example::onIdle() {
+    std::vector<bool> keyStates = this->getWindow()->getKeyboardState();
+
+    if (keyStates[KEY_ESC]) {
+        this->exit(0);
     }
 
-    return 0;
+    float correction = this->getFrameTime() * MOVE_SPEED;
+    Math::Vec3 forward(this->camera->getTarget() * correction);
+    Math::Vec3 right(this->camera->getRight() * correction);
+
+    if (keyStates[KEY_W]) {
+        this->player->move(forward);
+    }
+
+    if (keyStates[KEY_S]) {
+        this->player->move(-forward);
+    }
+
+    if (keyStates[KEY_D]) {
+        this->player->move(right);
+    }
+
+    if (keyStates[KEY_A]) {
+        this->player->move(-right);
+    }
 }
---
+++ example/src/Example.h
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2013 Pavlo Lavrenenko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef EXAMPLE_H
+#define EXAMPLE_H
+
+#include <Engine.h>
+#include <SceneNode.h>
+#include <Camera.h>
+#include <memory>
+
+#define KEY_ESC 41
+#define KEY_W   26
+#define KEY_S   22
+#define KEY_A   4
+#define KEY_D   7
+
+#define MOVE_SPEED  2.0f
+#define MOUSE_SPEED 0.1f
+
+class Example: public Graphene::Engine {
+public:
+    Example(int width, int height);
+
+private:
+    void onMouseMotion(int x, int y);
+    void onIdle();
+
+    std::shared_ptr<Graphene::SceneNode> player;
+    std::shared_ptr<Graphene::Camera> camera;
+};
+
+#endif  // EXAMPLE_H
