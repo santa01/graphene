5918ffd8d104cd020d250f9c77ad5d4621716bf5
Pavlo Lavrenenko <santa.ssh@gmail.com>
2015-06-14 21:23:26 +0300

    Bump version to 0.1.5

--- blender/graphene_entity.py
+++ blender/graphene_entity.py
@@ -23,7 +23,7 @@
 bl_info = {
     "name": "Graphene entity format",
     "author": "Pavlo Lavrenenko",
-    "version": (0, 1, 4),
+    "version": (0, 1, 5),
     "blender": (2, 6, 0),
     "location": "File > Import-Export",
     "description": "Export entity for Graphene library",
