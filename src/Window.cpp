48f5dbb3623dc32efa83dc76ab4158932876e694
Pavlo Lavrenenko <santa.ssh@gmail.com>
2019-05-14 17:38:05 +0300

    Move OpenGL initialization back to Engine as in master branch

--- src/Window.cpp
+++ src/Window.cpp
@@ -2,35 +2,9 @@
 #include <OpenGL.h>
 #include <windowsx.h>
 #include <stdexcept>
-#include <iostream>
-#include <string>
-#include <unordered_map>
 #include <algorithm>
 
 namespace Graphene {
-
-const std::unordered_map<GLenum, std::string> debugSource = {
-    { GL_DEBUG_SOURCE_API_ARB,             "OpenGL" },
-    { GL_DEBUG_SOURCE_WINDOW_SYSTEM_ARB,   "Window System" },
-    { GL_DEBUG_SOURCE_SHADER_COMPILER_ARB, "GLSL" },
-    { GL_DEBUG_SOURCE_THIRD_PARTY_ARB,     "3rd Party" },
-    { GL_DEBUG_SOURCE_APPLICATION_ARB,     "Application" },
-    { GL_DEBUG_SOURCE_OTHER_ARB,           "Other" }
-};
-
-const std::unordered_map<GLenum, std::string> debugType = {
-    { GL_DEBUG_TYPE_ERROR_ARB,               "Error" },
-    { GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR_ARB, "Deprecated" },
-    { GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR_ARB,  "Undefined" },
-    { GL_DEBUG_TYPE_PORTABILITY_ARB,         "Portability" },
-    { GL_DEBUG_TYPE_PERFORMANCE_ARB,         "Performance" },
-    { GL_DEBUG_TYPE_OTHER_ARB,               "Other" }
-};
-
-extern "C" void debugHandler(
-        GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const GLvoid* userParam) {
-    std::cout << debugType.at(type) << " [" << debugSource.at(source) << "]: " << message << std::endl;
-}
 
 #define SetWindowObject(window, object) SetWindowLongPtr((window), GWLP_USERDATA, reinterpret_cast<LONG_PTR>((object)))
 #define GetWindowObject(window) reinterpret_cast<Window*>(GetWindowLongPtr((window), GWLP_USERDATA))
@@ -131,23 +105,6 @@ void Window::createContext() {
     this->destroyWindow(dummyWindow);
 
     this->createExtContext(this->window);
-
-    OpenGL::loadCore();
-    OpenGL::loadExt();
-
-    glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS_ARB);
-    glDebugMessageControlARB(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, nullptr, true);
-    glDebugMessageCallbackARB(debugHandler, nullptr);
-
-    std::cout
-        << "Vendor: " << glGetString(GL_VENDOR) << std::endl
-        << "Renderer: " << glGetString(GL_RENDERER) << std::endl
-        << "OpenGL Version: " << glGetString(GL_VERSION) << std::endl
-        << "GLSL Version: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << std::endl;
-
-    glFrontFace(GL_CW);
-    glBlendFunc(GL_ONE, GL_ONE);
-    glEnable(GL_CULL_FACE);
 }
 
 void Window::destroyContext() {
